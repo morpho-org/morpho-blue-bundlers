485b5d5cb45b99eecb8f39f42fc40d70fcbccec3
MerlinEgalite <egalite.merlin@gmail.com>
2023-08-15 11:37:34 +0200

    perf: morpho-utils delegatecall

--- contracts/BaseSelfMulticall.sol
+++ contracts/BaseSelfMulticall.sol
@@ -1,28 +1,20 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.21;
 
+import "@morpho-utils/DelegateCall.sol";
+
 /// @title BaseSelfMulticall
 /// @notice Enables calling multiple functions in a single call to the same contract (self).
 abstract contract BaseSelfMulticall {
+    using DelegateCall for address;
+
     /* INTERNAL */
 
     function _multicall(bytes[] memory data) internal returns (bytes[] memory results) {
         results = new bytes[](data.length);
 
         for (uint256 i; i < data.length; ++i) {
-            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
-
-            if (!success) {
-                if (result.length < 68) revert();
-
-                assembly {
-                    result := add(result, 0x04)
-                }
-
-                revert(abi.decode(result, (string)));
-            }
-
-            results[i] = result;
+            results[i] = address(this).functionDelegateCall(data[i]);
         }
     }
 }
