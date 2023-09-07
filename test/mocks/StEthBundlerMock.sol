89871230b1187ccb7c11d3b1297995cf3af95236
Jean-Grimal <jeanbgrimal@gmail.com>
2023-09-07 15:53:32 +0200

    fix: lint

--- test/mocks/StEthBundlerMock.sol
+++ test/mocks/StEthBundlerMock.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.8.21;
 import "contracts/ethereum-mainnet/StEthBundler.sol";
 import "contracts/ERC20Bundler.sol";
 
-contract StEthBundlerMock is StEthBundler, ERC20Bundler {
-}
+contract StEthBundlerMock is StEthBundler, ERC20Bundler {}
