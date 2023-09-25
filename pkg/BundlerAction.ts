1eab2fe4453c7b6a91468d4ab38dd821b6019ecd
Rubilmax <rmilon@gmail.com>
2023-09-25 17:27:27 +0200

    fix(pkg): adapt interface

--- pkg/BundlerAction.ts
+++ pkg/BundlerAction.ts
@@ -49,7 +49,13 @@ export class BundlerAction {
 
   /* Permit */
 
-  static permit(asset: string, amount: BigNumberish, deadline: BigNumberish, signature: Signature): BundlerCall {
+  static permit(
+    asset: string,
+    amount: BigNumberish,
+    deadline: BigNumberish,
+    signature: Signature,
+    allowRevert: boolean,
+  ): BundlerCall {
     return BundlerAction.PERMIT_BUNDLER_IFC.encodeFunctionData("permit", [
       asset,
       amount,
@@ -57,17 +63,25 @@ export class BundlerAction {
       signature.v,
       signature.r,
       signature.s,
+      allowRevert,
     ]);
   }
 
   /* Permit2 */
 
-  static approve2(asset: string, amount: BigNumberish, deadline: BigNumberish, signature: Signature): BundlerCall {
+  static approve2(
+    asset: string,
+    amount: BigNumberish,
+    deadline: BigNumberish,
+    signature: Signature,
+    allowRevert: boolean,
+  ): BundlerCall {
     return BundlerAction.PERMIT2_BUNDLER_IFC.encodeFunctionData("approve2", [
       asset,
       amount,
       deadline,
       { v: signature.v, r: signature.r, s: signature.s },
+      allowRevert,
     ]);
   }
 
@@ -95,10 +109,15 @@ export class BundlerAction {
 
   /* Morpho */
 
-  static morphoSetAuthorizationWithSig(authorization: AuthorizationStruct, signature: Signature): BundlerCall {
+  static morphoSetAuthorizationWithSig(
+    authorization: AuthorizationStruct,
+    signature: Signature,
+    allowRevert: boolean,
+  ): BundlerCall {
     return BundlerAction.MORPHO_BUNDLER_IFC.encodeFunctionData("morphoSetAuthorizationWithSig", [
       authorization,
       { v: signature.v, r: signature.r, s: signature.s },
+      allowRevert,
     ]);
   }
 
@@ -202,8 +221,16 @@ export class BundlerAction {
     reward: string,
     amount: BigNumberish,
     proof: string[],
+    allowRevert: boolean,
   ): BundlerCall {
-    return BundlerAction.URD_BUNDLER_IFC.encodeFunctionData("urdClaim", [distributor, account, reward, amount, proof]);
+    return BundlerAction.URD_BUNDLER_IFC.encodeFunctionData("urdClaim", [
+      distributor,
+      account,
+      reward,
+      amount,
+      proof,
+      allowRevert,
+    ]);
   }
 
   /* Wrapped Native */
