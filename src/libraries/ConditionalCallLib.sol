fe6b6fca2f60f845d43a49b01e1f8520b4206ede
MerlinEgalite <egalite.merlin@gmail.com>
2023-09-25 19:13:20 +0200

    feat: add do + remove return vars

--- src/libraries/ConditionalCallLib.sol
+++ src/libraries/ConditionalCallLib.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.0;
 
 import {Address} from "@openzeppelin/utils/Address.sol";
 
+/// @title ConditionalCallLib
+/// @author Morpho Labs
+/// @custom:contact security@morpho.org
+/// @notice Library allowing to perform calls while allowing or not reverts.
 library ConditionalCallLib {
-    function conditionalCall(address target, bytes memory data, bool allowRevert)
-        internal
-        returns (bool callSuccess, bytes memory returnData)
-    {
+    /// @notice Calls the given `target` with `data` and reverts if `allowRevert` is false and the call reverts.
+    function conditionalCall(address target, bytes memory data, bool allowRevert) internal {
         (bool success, bytes memory returndata) = target.call(data);
+
         if (!allowRevert) {
             Address.verifyCallResultFromTarget(target, success, returndata, "Address: low-level call failed");
         }
