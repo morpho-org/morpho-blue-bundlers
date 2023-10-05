28fdcfb98a8e47fc36c2650a8fd2ab6985ebdc7d
Rubilmax <rmilon@gmail.com>
2023-10-05 11:19:40 +0200

    feat(multicall): prevent double initiation

---
+++ test/forge/BaseBundlerLocalTest.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {ErrorsLib} from "src/libraries/ErrorsLib.sol";
+
+import "src/mocks/bundlers/BaseBundlerMock.sol";
+
+import "./helpers/LocalTest.sol";
+
+contract BaseBundlerLocalTest is LocalTest {
+    function setUp() public override {
+        super.setUp();
+
+        bundler = new BaseBundlerMock();
+    }
+
+    function testMulticallEmpty() public {
+        bundler.multicall(bundle);
+    }
+
+    function testNestedMulticall() public {
+        bundle.push(abi.encodeCall(BaseBundler.multicall, (callbackBundle)));
+
+        vm.expectRevert(bytes(ErrorsLib.ALREADY_INITIATED));
+        bundler.multicall(bundle);
+    }
+}
