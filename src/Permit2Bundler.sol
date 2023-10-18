4ff5afbe36973a148cf6abd77f9129d7b95e163c
Jean-Grimal <jeanbgrimal@gmail.com>
2023-10-18 15:10:04 +0200

    feat: cherry pick 1

--- src/Permit2Bundler.sol
+++ src/Permit2Bundler.sol
@@ -20,6 +20,8 @@ abstract contract Permit2Bundler is BaseBundler {
     /// @notice Permits and performs a transfer from the initiator to the recipient via Permit2.
     /// @notice Warning: should only be called via the bundler's `multicall` function.
     /// @dev Pass `permit.permitted.amount = type(uint256).max` to transfer all.
+    /// @param permit The PermitTransferFrom struct.
+    /// @param signature The signature.
     function permit2TransferFrom(ISignatureTransfer.PermitTransferFrom memory permit, bytes memory signature)
         external
         payable
--- src/StEthBundler.sol
+++ src/StEthBundler.sol
@@ -28,6 +28,8 @@ abstract contract StEthBundler is BaseBundler {
     /* CONSTRUCTOR */
 
     /// @dev Warning: assumes the given addresses are non-zero (they are not expected to be deployment arguments).
+    /// @param stEth The address of the ST_ETH contract.
+    /// @param wstEth The address of the WST_ETH contract.
     constructor(address stEth, address wstEth) {
         ST_ETH = stEth;
         WST_ETH = wstEth;
@@ -39,6 +41,8 @@ abstract contract StEthBundler is BaseBundler {
 
     /// @notice Stakes the given `amount` of ETH via Lido, using the `referral` id.
     /// @dev Pass `amount = type(uint256).max` to stake all.
+    /// @param amount The amount of Eth to stake.
+    /// @param referral The address of the referral regarding the Lido Rewards-Share Program.
     function stakeEth(uint256 amount, address referral) external payable {
         amount = Math.min(amount, address(this).balance);
 
@@ -48,6 +52,7 @@ abstract contract StEthBundler is BaseBundler {
 
     /// @notice Wraps the given `amount` of stETH to wstETH.
     /// @dev Pass `amount = type(uint256).max` to wrap all.
+    /// @param amount The amount of stEth to wrap.
     function wrapStEth(uint256 amount) external payable {
         amount = Math.min(amount, ERC20(ST_ETH).balanceOf(address(this)));
 
@@ -58,6 +63,7 @@ abstract contract StEthBundler is BaseBundler {
 
     /// @notice Unwraps the given `amount` of wstETH to stETH.
     /// @dev Pass `amount = type(uint256).max` to unwrap all.
+    /// @param amount The amount of wstEth to unwrap.
     function unwrapStEth(uint256 amount) external payable {
         amount = Math.min(amount, ERC20(WST_ETH).balanceOf(address(this)));
 
