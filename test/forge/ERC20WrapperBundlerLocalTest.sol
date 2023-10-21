895a1c62107cd37a0600b46861c8404a95648f5e
MerlinEgalite <egalite.merlin@gmail.com>
2023-10-21 20:07:36 +0300

    test: add tests

---
+++ test/forge/ERC20WrapperBundlerLocalTest.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {ErrorsLib} from "../../src/libraries/ErrorsLib.sol";
+
+import {ERC20WrapperBundlerMock} from "../../src/mocks/bundlers/ERC20WrapperBundlerMock.sol";
+import {ERC20WrapperMock} from "../../src/mocks/ERC20WrapperMock.sol";
+
+import "./helpers/LocalTest.sol";
+
+contract ERC20WrapperBundlerBundlerLocalTest is LocalTest {
+    ERC20WrapperMock internal loanWrapper;
+
+    function setUp() public override {
+        super.setUp();
+
+        bundler = new ERC20WrapperBundlerMock();
+
+        loanWrapper = new ERC20WrapperMock(loanToken, "Wrapped Loan Token", "WLT");
+    }
+
+    function testDepositFor(uint256 amount) public {
+        amount = bound(amount, MIN_AMOUNT, MAX_AMOUNT);
+
+        bundle.push(_erc20WrapperDepositFor(address(loanWrapper), RECEIVER, amount));
+
+        loanToken.setBalance(address(bundler), amount);
+
+        bundler.multicall(bundle);
+
+        assertEq(loanToken.balanceOf(address(bundler)), 0, "loan.balanceOf(bundler)");
+        assertEq(loanWrapper.balanceOf(RECEIVER), amount, "loanWrapper.balanceOf(RECEIVER)");
+    }
+
+    function testDepositForAssetZeroAddress(uint256 amount) public {
+        amount = bound(amount, MIN_AMOUNT, MAX_AMOUNT);
+
+        bundle.push(_erc20WrapperDepositFor(address(0), RECEIVER, amount));
+
+        vm.expectRevert(bytes(ErrorsLib.ZERO_ADDRESS));
+        bundler.multicall(bundle);
+    }
+
+    function testDepositForAccountZeroAddress(uint256 amount) public {
+        amount = bound(amount, MIN_AMOUNT, MAX_AMOUNT);
+
+        bundle.push(_erc20WrapperDepositFor(address(loanWrapper), address(0), amount));
+
+        vm.expectRevert(bytes(ErrorsLib.ZERO_ADDRESS));
+        bundler.multicall(bundle);
+    }
+
+    function testDepositForZeroAmount() public {
+        bundle.push(_erc20WrapperDepositFor(address(loanWrapper), RECEIVER, 0));
+
+        vm.expectRevert(bytes(ErrorsLib.ZERO_AMOUNT));
+        bundler.multicall(bundle);
+    }
+
+    function testWithdrawTo(uint256 amount) public {
+        amount = bound(amount, MIN_AMOUNT, MAX_AMOUNT);
+
+        loanWrapper.setBalance(address(bundler), amount);
+        loanToken.setBalance(address(loanWrapper), amount);
+
+        bundle.push(_erc20WrapperWithdrawTo(address(loanWrapper), RECEIVER, amount));
+
+        bundler.multicall(bundle);
+
+        assertEq(loanWrapper.balanceOf(address(bundler)), 0, "loanWrapper.balanceOf(bundler)");
+        assertEq(loanToken.balanceOf(RECEIVER), amount, "loan.balanceOf(RECEIVER)");
+    }
+
+    function testWithdrawToAssetZeroAddress(uint256 amount) public {
+        amount = bound(amount, MIN_AMOUNT, MAX_AMOUNT);
+
+        bundle.push(_erc20WrapperWithdrawTo(address(0), RECEIVER, amount));
+
+        vm.expectRevert(bytes(ErrorsLib.ZERO_ADDRESS));
+        bundler.multicall(bundle);
+    }
+
+    function testWithdrawToAccountZeroAddress(uint256 amount) public {
+        amount = bound(amount, MIN_AMOUNT, MAX_AMOUNT);
+
+        bundle.push(_erc20WrapperWithdrawTo(address(loanWrapper), address(0), amount));
+
+        vm.expectRevert(bytes(ErrorsLib.ZERO_ADDRESS));
+        bundler.multicall(bundle);
+    }
+
+    function testWithdrawToZeroAmount() public {
+        bundle.push(_erc20WrapperWithdrawTo(address(loanWrapper), RECEIVER, 0));
+
+        vm.expectRevert(bytes(ErrorsLib.ZERO_AMOUNT));
+        bundler.multicall(bundle);
+    }
+}
