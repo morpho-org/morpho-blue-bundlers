811df2dfd85161451724ccab4a7f41a31b167ad4
Jean-Grimal <jeanbgrimal@gmail.com>
2023-10-23 12:18:14 +0200

    feat: add interfaces natspec

--- src/MorphoBundler.sol
+++ src/MorphoBundler.sol
@@ -207,13 +207,13 @@ abstract contract MorphoBundler is BaseBundler, IMorphoBundler {
     }
 
     /// @notice Triggers a flash loan on Morpho.
-    /// @param asset The address of the token to flash loan.
-    /// @param amount The amount of assets to flash loan.
+    /// @param token The address of the token to flash loan.
+    /// @param asset The amount of assets to flash loan.
     /// @param data Arbitrary data to pass to the `onMorphoFlashLoan` callback.
-    function morphoFlashLoan(address asset, uint256 amount, bytes calldata data) external payable {
-        _approveMaxMorpho(asset);
+    function morphoFlashLoan(address token, uint256 asset, bytes calldata data) external payable {
+        _approveMaxMorpho(token);
 
-        MORPHO.flashLoan(asset, amount, data);
+        MORPHO.flashLoan(token, asset, data);
     }
 
     /* INTERNAL */
--- src/interfaces/IMulticall.sol
+++ src/interfaces/IMulticall.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
+/// @title IMulticall
+/// @author Morpho Labs
+/// @custom:contact security@morpho.org
+/// @notice Interface of Multicall.
 interface IMulticall {
     function multicall(bytes[] calldata data) external payable;
 }
