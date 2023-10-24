afda46d43f0ef192987edbee7d306e449b7cee0c
Jean-Grimal <jeanbgrimal@gmail.com>
2023-10-24 10:52:52 +0200

    fix: skip-revert-documentation-issue-19

--- src/PermitBundler.sol
+++ src/PermitBundler.sol
@@ -13,7 +13,7 @@ abstract contract PermitBundler is BaseBundler {
     /// @notice Permits the given `amount` of `asset` from sender to be spent by the bundler via EIP-2612 Permit with
     /// the given `deadline` & EIP-712 signature's `v`, `r` & `s`.
     /// @notice Warning: should only be called via the bundler's `multicall` function.
-    /// @dev Pass `skipRevert == true` to avoid reverting the whole bundle in case the signature expired.
+    /// @dev Pass `skipRevert = true` to avoid reverting the call in case the signature is frontrunned.
     function permit(address asset, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s, bool skipRevert)
         external
         payable
