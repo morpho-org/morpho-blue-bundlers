e571e702c99720374f3885e7ca07ed66e725b3bf
Jean-Grimal <jeanbgrimal@gmail.com>
2023-10-24 14:52:44 +0200

    refactor: remove steth from constructor arguments

--- src/ethereum/libraries/MainnetLib.sol
+++ src/ethereum/libraries/MainnetLib.sol
@@ -5,9 +5,6 @@ library MainnetLib {
     /// @dev The address of the WETH contract on Ethereum.
     address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    /// @dev The address of the stETH contract on Ethereum.
-    address internal constant ST_ETH = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
-
     /// @dev The address of the wstETH contract on Ethereum.
     address internal constant WST_ETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
 
