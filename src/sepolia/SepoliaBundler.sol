90837751cf3b819715f39bd22d79c342424e9761
Rubilmax <rmilon@gmail.com>
2023-12-15 09:12:52 +0100

    feat(bundler): add Sepolia bundler

---
+++ src/sepolia/SepoliaBundler.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.21;
+
+import {GoerliLib} from "./libraries/GoerliLib.sol";
+
+import {BaseBundler} from "../BaseBundler.sol";
+import {TransferBundler} from "../TransferBundler.sol";
+import {PermitBundler} from "../PermitBundler.sol";
+import {Permit2Bundler} from "../Permit2Bundler.sol";
+import {ERC4626Bundler} from "../ERC4626Bundler.sol";
+import {WNativeBundler} from "../WNativeBundler.sol";
+import {UrdBundler} from "../UrdBundler.sol";
+import {MorphoBundler} from "../MorphoBundler.sol";
+import {ERC20WrapperBundler} from "../ERC20WrapperBundler.sol";
+
+/// @title SepoliaBundler
+/// @author Morpho Labs
+/// @custom:contact security@morpho.org
+/// @notice Bundler contract specific to the Sepolia testnet.
+contract SepoliaBundler is
+    TransferBundler,
+    PermitBundler,
+    Permit2Bundler,
+    ERC4626Bundler,
+    WNativeBundler,
+    UrdBundler,
+    MorphoBundler,
+    ERC20WrapperBundler
+{
+    /* CONSTRUCTOR */
+
+    constructor(address morpho) WNativeBundler(GoerliLib.WETH) MorphoBundler(morpho) {}
+
+    /* INTERNAL */
+
+    /// @inheritdoc MorphoBundler
+    function _isSenderAuthorized() internal view override(BaseBundler, MorphoBundler) returns (bool) {
+        return MorphoBundler._isSenderAuthorized();
+    }
+}
---
+++ src/sepolia/libraries/SepoliaLib.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+library SepoliaLib {
+    /// @dev The address of the WETH contract on Ethereum.
+    address internal constant WETH = 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9;
+}
