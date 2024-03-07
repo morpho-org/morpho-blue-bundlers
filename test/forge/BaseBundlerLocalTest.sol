93cbe0833e9ba2d7eb85b4f2dab0f7e09c25f25c
MathisGD <mathis@morpho.xyz>
2024-03-07 19:31:32 +0100

    test: unit tests for the base bundler

--- test/forge/BaseBundlerLocalTest.sol
+++ test/forge/BaseBundlerLocalTest.sol
@@ -25,3 +25,27 @@ contract BaseBundlerLocalTest is LocalTest {
         bundler.multicall(bundle);
     }
 }
+
+contract BaseBundlerLocalEnshrinedTest is BaseBundler, LocalTest {
+    function checkInitiator(address expectedInitiator) public payable protected {
+        require(initiator() == expectedInitiator, "unexpected initiator");
+    }
+
+    function revertWith(string memory data) public payable protected {
+        revert(data);
+    }
+
+    function testMulticallShouldSetTheRightInitiator(address caller) public {
+        bundle.push(abi.encodeCall(this.checkInitiator, (caller)));
+
+        vm.prank(caller);
+        this.multicall(bundle);
+    }
+
+    function testMulticallShouldPassRevertData(string memory data) public {
+        bundle.push(abi.encodeCall(this.revertWith, (data)));
+
+        vm.expectRevert(bytes(data));
+        this.multicall(bundle);
+    }
+}
