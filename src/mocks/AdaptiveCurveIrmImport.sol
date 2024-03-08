69f49482ce8bdf142a29379b47f3c53772ddc273
MathisGD <74971347+MathisGD@users.noreply.github.com>
2024-03-08 12:31:16 +0100

    chore: strict pragma morpho import
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/mocks/AdaptiveCurveIrmImport.sol
+++ src/mocks/AdaptiveCurveIrmImport.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 // Force foundry to compile this contract.
 // The resulting bytecode is then loaded by the tests.
 
