e8293fec07f23236e032be8f3d8a7c493b876913
MathisGD <mathis@morpho.xyz>
2024-03-11 12:10:20 +0100

    test: minor improvements

---
+++ test/forge/BaseBundlerEnshrinedLocalTest.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {BaseBundler} from "src/mocks/bundlers/BaseBundlerMock.sol";
+
+import "./helpers/LocalTest.sol";
+
+contract BaseBundlerLocalEnshrinedTest is BaseBundler, LocalTest {
+    function checkInitiator(address expectedInitiator) public payable protected {
+        require(initiator() == expectedInitiator, "unexpected initiator");
+    }
+
+    function revertWith(string memory data) public payable protected {
+        revert(data);
+    }
+
+    function testMulticallShouldSetTheRightInitiator(address caller) public {
+        bundle.push(abi.encodeCall(this.checkInitiator, (caller)));
+
+        vm.prank(caller);
+        this.multicall(bundle);
+    }
+
+    function testMulticallShouldPassRevertData(string memory data) public {
+        bundle.push(abi.encodeCall(this.revertWith, (data)));
+
+        vm.expectRevert(bytes(data));
+        this.multicall(bundle);
+    }
+}
